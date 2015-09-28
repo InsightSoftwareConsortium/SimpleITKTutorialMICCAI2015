feb5672a29e72ddd91208913fce9eaa5129c403e
Matt McCormick <matt.mccormick@kitware.com>
2015-09-27 20:53:40 -0400

    Add test.sh.
    This is used to test that all the notebooks execute successfully.

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+thisdir="${BASH_SOURCE%/*}"
+
+tempoutput=`tempfile --suffix=.ipynb`
+trap 'rm -f $MYTEMP' SIGINT SIGTERM
+
+failed=0
+while read notebook; do
+  echo $notebook | grep -q ipynb_checkpoints && continue
+  echo "Testing ${notebook}..."
+  jupyter nbconvert --to notebook "$notebook" --output $tempoutput --execute
+  if test $? -ne 0; then
+    echo "Error: $(basename "$notebook") failed to execute." 1>&2
+    let "failed++"
+  fi
+done <<< "$(find "$thisdir" -name '*.ipynb')"
+
+rm $tempoutput
+echo "$failed notebooks failed to execute."
+exit $failed
