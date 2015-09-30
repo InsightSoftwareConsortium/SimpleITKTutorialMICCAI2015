85f103edaec0ba62a6e720c23aab8e235b3b6ea0
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-09-30 11:18:51 -0400

    Start a docker machine if needed and display URL hosting notebook

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+_MACHINE_NAME=$(docker-machine active 2> /dev/null || echo "default")
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# if we need to use docker machine let us start the "default"
+if ! docker info &> /dev/null; then
+    command -v docker-machine &> /dev/null || (echo "Docker is not configured correctly. No host or docker-machine." && exit 1)
+    if [ "$(docker-machine status $_MACHINE_NAME 2> /dev/null)" != "Running" ]; then
+        echo "Starting machine $_MACHINE_NAME..."
+        docker-machine start $_MACHINE_NAME || exit 1
+    fi
+    eval $(docker-machine env $_MACHINE_NAME --shell=sh)
+fi
+
+_MACHINE_IP=$(docker-machine ip ${_MACHINE_NAME} 2> /dev/null || echo "localhost" )
+_URL="http://${_MACHINE_IP}:8888"
+
+echo "\n\n${GREEN}Setting up Docker Jupyter Notebook at ${_URL}${NC}\n\n"
+
+
 docker run \
   --rm \
   -p 8888:8888 \
